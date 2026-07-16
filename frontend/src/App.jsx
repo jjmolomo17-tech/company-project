fb27554bf5ac6e3efbc4dd0f680912708a5e53b4
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-16 18:04:34 +0200

    App.jsx imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
+import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+import Partners from "./components/Partners/Partners.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+import headphone from "./assets/hero/headphone.png";
+import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png";
+
+import Products from "./components/Products/Products";
+import Blogs from "./components/Blogs/Blogs";
+import Footer from "./components/Footer/Footer.jsx";
+import Popup from "./components/Popup/Popup.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37",
+};
+
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
+
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+
+  React.useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+      offset: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Category />
+      <Category2 />
+      <Services />
+      <Banner data={BannerData} />
+      <Products />
+      <Banner data={BannerData2} />
+      <Blogs />
+      <Partners />
+      <Footer />
+      <Popup orderPopup={orderPopup} handleOrderPopup={handleOrderPopup} />
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
